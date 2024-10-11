79eb51be23f5441ef9d707d60566a206cc828b29
Juan Villa <jujotovil@gmail.com>
2024-10-11 10:33:56 -0500

    detailing cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 
 import './App.css'
+import {MainPage__cards} from './components/Mainpage__cards';
 
 function App() {
   
 
   return (
     <>
+      
+
+      <div className='cards__container'>
+        <MainPage__cards/>
+        <MainPage__cards/>
+        <MainPage__cards/>
+      </div>
+
+
 
     </>
   )
